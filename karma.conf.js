fc188913ffe11f0cc287581a2546775f1c4349f1
marc.luettecke <marc.luettecke@uni-konstanz.de>
2021-05-04 17:01:23 +0200

    updated project name

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/weather-app'),
+      dir: require('path').join(__dirname, './coverage/weatherApp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
