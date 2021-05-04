e245d746923ce7c5933489d353f81b7fad415088
marc.luettecke <marc.luettecke@uni-konstanz.de>
2021-05-04 15:25:33 +0200

    added env variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+	production: true,
+	apiKey: 'a3ff900fb3e7fc2b51ea1b5f84d99dca',
+	firebase: {
+		apiKey: 'AIzaSyAFoAYFsonsjmIqHWmIHqMYYhXUF3r9HQo',
+		authDomain: 'ng-weather-app-6f1dd.firebaseapp.com',
+		projectId: 'ng-weather-app-6f1dd',
+		storageBucket: 'ng-weather-app-6f1dd.appspot.com',
+		messagingSenderId: '766306356934',
+		appId: '1:766306356934:web:b78e3ed5dd3ecfa3b911c7',
+		measurementId: 'G-10N0292Q41'
+	}
+	// apiKey: Fill in your API key here for the environment.ts file
+};
